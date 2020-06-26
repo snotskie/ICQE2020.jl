7c75070b9af19bf5e02773c379f67e043f4046a3
Bryan A. Knowles <snotskie@gmail.com>
2020-06-26 16:04:58 -0500

    changes

--- main.jl
+++ main.jl
@@ -52,7 +52,7 @@ display(unique(TADMUS_Data[!, conversations]))
 
 # Running first ENA
 myRotation = EpistemicNetworkAnalysis.Formula2Rotation(
-    MixedModel, 2, @formula(y ~ 1 + DummyMacroRole + Team + DummyMacroRole&Team + (1 + DummyMacroRole | Team)),
+    MixedModel, 2, @formula(y ~ 1 + DummyMacroRole + Team + DummyMacroRole&Team + (1|Team) + (DummyMacroRole|Team)),
     LinearModel, 2, @formula(y ~ 1 + DummyCOND)
 )
 
@@ -72,6 +72,7 @@ myArtist = EpistemicNetworkAnalysis.TVRemoteArtist(
 
 p1 = plot(myENA,
     artist=myArtist,
+    showconfidence=true,
     yaxisname="Control/Treatment",
     xaxisname="Support/Command"
 )
@@ -79,26 +80,10 @@ p1 = plot(myENA,
 k = 1
 xlims!(p1, -k, k)
 ylims!(p1, -k, k)
-title!(p1, "Controlling for Nesting in Teams")
-savefig(p1, "units-controlled.svg")
-savefig(p1, "units-controlled.png")
+title!(p1, "Controlled Model")
+savefig(p1, "controlled.svg")
+savefig(p1, "controlled.png")
 display(p1)
-
-p2 = plot(myENA,
-    showunits=false,
-    showconfidence=true,
-    artist=myArtist,
-    yaxisname="Control/Treatment",
-    xaxisname="Support/Command"
-)
-
-k = 1
-xlims!(p2, -k, k)
-ylims!(p2, -k, k)
-title!(p2, "Controlling for Nesting in Teams")
-savefig(p2, "means-controlled.svg")
-savefig(p2, "means-controlled.png")
-display(p2)
 
 # Running second ENA
 myRotation = EpistemicNetworkAnalysis.Formula2Rotation(
@@ -115,32 +100,20 @@ myENA = ENAModel(TADMUS_Data, codes, conversations, units,
 display(myENA)
 
 # Plotting
-p1 = plot(myENA,
-    artist=myArtist,
-    yaxisname="Control/Treatment",
-    xaxisname="Support/Command"
-)
-
-k = 1
-xlims!(p1, -k, k)
-ylims!(p1, -k, k)
-title!(p1, "Not Controlling for Nesting in Teams")
-savefig(p1, "units-uncontrolled.svg")
-savefig(p1, "units-uncontrolled.png")
-display(p1)
-
 p2 = plot(myENA,
-    showunits=false,
-    showconfidence=true,
     artist=myArtist,
+    showconfidence=true,
     yaxisname="Control/Treatment",
-    xaxisname="Support/Command"
+    xaxisname="Support/Command",
+    subsetFilter=TADMUS_Row -> TADMUS_Row[:MacroRole] in ["Command"]
 )
 
 k = 1
 xlims!(p2, -k, k)
 ylims!(p2, -k, k)
-title!(p2, "Not Controlling for Nesting in Teams")
-savefig(p2, "means-uncontrolled.svg")
-savefig(p2, "means-uncontrolled.png")
+title!(p2, "Univariate Model")
+savefig(p2, "uncontrolled.svg")
+savefig(p2, "uncontrolled.png")
 display(p2)
+
+# TODO run each model on just one of the two groups
